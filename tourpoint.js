86dbc4b9c52ee8270bc6200b64470a429bea5161
Nutthagarn267 <124670673+Nutthagarn267@users.noreply.github.com>
2023-05-29 11:16:22 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,146 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "หน่องริมคลอง",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.5886845869478,
+          13.741188282629636
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ร้านอาหารยศเส",
+        "level": 8
+      },
+      "geometry": {
+        "coordinates": [
+          100.51425427116112,
+          13.749190789298638
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ท่าเตียนโภชนา",
+        "level": 8
+      },
+      "geometry": {
+        "coordinates": [
+          100.49165900000014,
+          13.74546994607202
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ก๋วยเตี๋ยวเป็ด",
+        "level": 9
+      },
+      "geometry": {
+        "coordinates": [
+          100.52424099999962,
+          13.778852109624395
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Suki_Teenoi",
+        "level": 8
+      },
+      "geometry": {
+        "coordinates": [
+          100.40666572883543,
+          13.765442368976565
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ราดหน้ายอดผัก",
+        "level": 9
+      },
+      "geometry": {
+        "coordinates": [
+          100.41581200000138,
+          13.783187370164569
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Jae_Oh",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.52255499999814,
+          13.742330945723808
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เจ้อ้วนโภชนา",
+        "level": 8
+      },
+      "geometry": {
+        "coordinates": [
+          100.46325272883945,
+          13.72345521127454
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wang_Lang_Bakery",
+        "level": 9
+      },
+      "geometry": {
+        "coordinates": [
+          100.48631200000045,
+          13.755904368340268
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "1000_Zen",
+        "level": 8
+      },
+      "geometry": {
+        "coordinates": [
+          100.54479552806515,
+          13.780856602908983
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
+  
